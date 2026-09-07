00e1a6d4ad2b5f8a4450a8026c7bcefbd40c14bd
Hesham <hesham@192.168.1.40>
2026-09-07 22:39:03 +0300

    Use DATABASE_URL coneection starting for Neon

--- server/db.js
+++ server/db.js
@@ -1,8 +1,15 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'wajeez_db',
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+
+pool.on('connect', () => {
+  console.log('⚡ PostgreSQL الداتابيز متصل');
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
